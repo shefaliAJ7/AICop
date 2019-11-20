fe05be2a58dbc14af8eb3590bc46761ae9631f33
shefaliAJ7 <sanand22@asu.edu>
2019-11-20 00:24:14 -0700

    initial commit

---
+++ src/srs_aicop/SRS_AICop.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srs_aicop;
+
+/**
+ *
+ * @author Shefali Anand
+ */
+public class SRS_AICop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AICopDisplay aicop = new AICopDisplay();
+    }
+    
+}
